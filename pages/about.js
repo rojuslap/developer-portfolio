640fb5a978ef885cef7bdfb4807544d45bef3a23
Rojus Lapinskas <rojuslapinskas@gmail.com>
2023-03-06 16:19:28 +0100

    button styling

--- pages/about.js
+++ pages/about.js
@@ -38,7 +38,7 @@ function about() {
             </p>
             <div className="mt-8 flex justify-center xl:justify-start">
               <a className="bg-neutral-900 text-white dark:bg-neutral-50 dark:text-black px-4 py-2 rounded-md">
-                Resume
+                Send me an email
               </a>
             </div>
           </div>
